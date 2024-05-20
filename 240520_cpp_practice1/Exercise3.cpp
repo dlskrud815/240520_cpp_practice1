9df1af2e28a3b447f8f9677a77c404e8bfcbf95b
dlskrud815 <dlskrud815@gmail.com>
2024-05-20 20:05:46 +0900

    Exercisd3

--- 240520_cpp_practice1/Exercise3.cpp
+++ 240520_cpp_practice1/Exercise3.cpp
@@ -66,35 +66,31 @@ int main()
 		cout << endl << "과자 바구니에 추가할 간식을 고르시오.( 1: 사탕, 2: 초콜릿, 0: 종료) : ";
 		cin >> option;
 
-		if (isdigit(option) == 0 && option >= 0 && option <= 2)
+		switch (option)
 		{
-			switch (option)
+		case 1:
+			cout << "맛을 입력하세요. : ";
+			cin >> input;
+			snackBasket.push_back(new Candy(input));
+			break;
+		case 2:
+			cout << "모양을 입력하세요. : ";
+			cin >> input;
+			snackBasket.push_back(new Chocolate(input));
+			break;
+		case 0:
+			cout << endl << "과자 바구니에 담긴 간식의 개수는 "<< Snack::snackCount << "개 입니다." << endl << endl;
+			cout << "과자 바구니에 담긴 간식 확인하기!" << endl;
+
+			for (auto snack : snackBasket)
 			{
-			case 1:
-				cout << "맛을 입력하세요. : ";
-				cin >> input;
-				snackBasket.push_back(new Candy(input));
-				break;
-			case 2:
-				cout << "모양을 입력하세요. : ";
-				cin >> input;
-				snackBasket.push_back(new Chocolate(input));
-				break;
-			case 0:
-				cout << endl << "과자 바구니에 담긴 간식의 개수는 "<< Snack::snackCount << "개 입니다." << endl << endl;
-				cout << "과자 바구니에 담긴 간식 확인하기!" << endl;
-
-				for (auto snack : snackBasket)
-				{
-					snack->printSnack();
-				}
-
-				return 0;
+				snack->printSnack();
 			}
-		}
-		else
-		{
+
+			return 0;
+		default:
 			cout << "0~2 사이의 숫자를 입력하세요." << endl;
+			break;
 		}
 	}
 }
