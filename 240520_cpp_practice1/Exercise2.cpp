2446739162e46a05799a4e8100077a080adefdca
dlskrud815 <dlskrud815@gmail.com>
2024-05-20 17:07:26 +0900

    override 추가

--- 240520_cpp_practice1/Exercise2.cpp
+++ 240520_cpp_practice1/Exercise2.cpp
@@ -21,7 +21,7 @@ public:
 		this->name = name;
 	}
 
-	void intro()
+	void intro() override
 	{
 		cout << name << "학생입니다." << endl;
 	}
@@ -42,7 +42,7 @@ public:
 		this->name = name;
 	}
 
-	void intro()
+	void intro() override
 	{
 		cout << name << "선생입니다." << endl;
 	}
